d92598c4cebf3d486dee17699d3230e83ec9eefa
Jan-Pakos <janpakos123@gmail.com>
2026-06-02 15:31:36 +0200

    add home and reservation modules with components and routing

---
+++ src/app/reservation/reservation-module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [
+    ReservationFormComponent,
+    ReservationListComponent,
+  ],
+  imports: [CommonModule],
+})
+export class ReservationModule {}
