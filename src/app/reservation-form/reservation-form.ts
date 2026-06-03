75207c9fbefa9d4efe27324afba41cbb61cd5ac8
Jan-Pakos <janpakos123@gmail.com>
2026-06-03 15:06:18 +0200

    refactor: enhance reservation form with reactive forms and validation

--- src/app/reservation-form/reservation-form.ts
+++ src/app/reservation-form/reservation-form.ts
@@ -1,16 +1,26 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reservation-form',
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './reservation-form.html',
   styleUrl: './reservation-form.css',
 })
 export class ReservationForm implements OnInit {
-  guestEmail = '';
+  reservationForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
 
   ngOnInit() {
+    this.reservationForm = this.fb.group({
+      guestEmail: ['', [Validators.required, Validators.email]],
+      guestName: ['', Validators.required],
+      checkInDate: ['', Validators.required],
+      checkOutDate: ['', Validators.required],
+      roomNumber: ['', [Validators.required, Validators.min(1)]]
+    });
   }
 
   onSubmit() {
